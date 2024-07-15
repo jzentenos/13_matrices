409c08417469957ccd89b86553d54f340bcc5f96
jzentenos <jzentenos@unjbg.edu.pe>
2024-07-15 10:28:52 -0500

    Ejecicio 1: Se agregó el hallazgo de la suma de los elementos de arriba de la diagonal principal.

--- matrices1.cpp
+++ matrices1.cpp
@@ -17,6 +17,14 @@ int main(){
 		}
 		cout<<endl;
 	}
-	
+	int s=0;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			if(i<j){
+			s+=A[i][j];
+			}
+		}
+	}
+	cout<<"Suma de elemetos arriba de la diagonal principal: "<<s;
 	return 0;
 }
